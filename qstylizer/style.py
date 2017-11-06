f7e60c6336d5409f37963cbb04bda95a0ca35b52
Brett Lambright <brettpl@gmail.com>
2017-11-05 20:02:50 -0800

    Update docstrings in qstylizer.style module

--- qstylizer/style.py
+++ qstylizer/style.py
@@ -38,7 +38,6 @@ class Style(collections.OrderedDict, qstylizer.setter.prop.PropSetter):
         """Determine subclass from string name.
 
         :param name: name of type string
-        :return: Style subclass
 
         """
         class_ = ClassStyle
@@ -72,48 +71,82 @@ class Style(collections.OrderedDict, qstylizer.setter.prop.PropSetter):
         """Override the retrieving of a value from dictionary.
 
         Find or create style in the key's hash location.
-        Split the requested key into individual components based on the _split_regex
-        and recursively build the hierarchy looping through the components.
-
-        If name is "QClass::subcontrol::pseudostate":
-            first_key is "QClass"
-            remaining is "::subcontrol::pseudostate"
 
         :param key: The dictionary key
 
         """
-        return self._find_or_create_value_from_key(key)
+        return self._find_or_create_value_from_name(key)
 
-    def _find_or_create_value_from_key(self, key):
-        key = key.replace("_", "-")
-        value = self._find_value_from_key(key)
+    def _find_or_create_value_from_name(self, name):
+        """Find or create a value from a string key.
+
+        If the key value already exists, return the value.
+        If there is a comma in requested key, return a StyleList object.
+        Otherwise create substyles from the style names in the key and return
+        the top level substyle or property.
+
+        :param name: The dictionary key
+
+        """
+        name = str(name).replace("_", "-")
+        value = self._find_value_from_name(name)
         if value is not None:
             return value
-        if "," in key:
-            style_list = self._create_substyle_list(key)
+        if "," in name:
+            style_list = self._create_substyle_list(name)
             return style_list
-        return self._create_substyles_from_names(key)
+        return self._create_substyles_from_name(name)
 
-    def _find_value_from_key(self, key):
+    def _find_value_from_name(self, key):
+        """Find value from key.
+
+        Simply return the key's hash value in the ordered dict.
+
+        """
         return self.get(key)
 
-    def _create_substyle_list(self, key):
-        style_list = StyleList(name=key, parent=self, is_root=False)
-        self.__setitem__(key, style_list)
+    def _create_substyle_list(self, name):
+        """Create a StyleList object and add it to ordered dict.
+
+        :param name: String name
+
+        """
+        style_list = StyleList(name=name, parent=self, is_root=False)
+        self.__setitem__(name, style_list)
         return style_list
 
-    def _create_substyles_from_names(self, names):
-        split_names = re.findall(self._split_regex, names)[:-1]
+    def _create_substyles_from_name(self, name):
+        """Create substyles from name.
+
+        Split the name into individual components based on the _split_regex
+        and recursively build the Style hierarchy looping through
+        the components.
+
+        If name is "QClass::subcontrol::pseudostate",
+        first_key is "QClass" and remaining is "::subcontrol::pseudostate"
+
+        :param name: String to split
+
+        """
+        split_names = re.findall(self._split_regex, name)[:-1]
         curr_key = split_names[0]
-        name = curr_key.replace(":", "")
-        remaining = names.split(curr_key, 1)[-1]
-        if self.get(name) is None:
+        first_name = curr_key.replace(":", "")
+        remaining = name.split(curr_key, 1)[-1]
+        if self.get(first_name) is None:
             self._create_substyle_from_name(curr_key)
         if remaining and remaining != curr_key:
-            return self._find_value_from_key(name).__getitem__(remaining)
-        return self._find_value_from_key(name)
+            return self._find_value_from_name(first_name).__getitem__(remaining)
+        return self._find_value_from_name(first_name)
 
     def _create_substyle_from_name(self, name):
+        """Create substyle from name.
+
+        Determine subclass from name, create an instance of the subclass,
+        then add it to ordered dict.
+
+        :param name: String name
+
+        """
         name_stripped = name.replace(":", "")
         class_ = self._subclass_from_name(name)
         new_style = class_(name=name_stripped, parent=self,
@@ -121,11 +154,27 @@ class Style(collections.OrderedDict, qstylizer.setter.prop.PropSetter):
         self.__setitem__(name_stripped, new_style)
 
     def __getattr__(self, name):
+        """Override the retrieving of the attribute.
+
+        If attribute starts with an underscore, return the attribute from
+        the object's __dict__ otherwise retrieve it from the ordered dict.
+
+        :param name: String name of attribute to retrieve
+
+        """
         if name.startswith("_"):
             return self.__getattribute__(name)
         return self.__getitem__(name)
 
     def __delattr__(self, name):
+        """Override the deleting of an attribute.
+
+        If attribute starts with an underscore, delete the attribute from
+        the object's __dict__ otherwise delete it from the ordered dict.
+
+        :param name: String name of attribute to delete
+
+        """
         if name in self.__dict__:
             return super(Style, self).__delattr__(name)
         return self.__delitem__(name)
@@ -138,14 +187,13 @@ class Style(collections.OrderedDict, qstylizer.setter.prop.PropSetter):
         name = name.replace('_', '-')
         return self.__setitem__(name, val)
 
-
-
     @property
     def identifier(self):
         """Get the identifier.
 
-        Example:
-            Object::subcontrol::pseudostate
+        Example::
+
+            Object::subcontrol:pseudostate
 
         """
         if not self._parent:
@@ -156,7 +204,7 @@ class Style(collections.OrderedDict, qstylizer.setter.prop.PropSetter):
     def name(self):
         """Return the name of the Style (eg. "QCheckBox").
 
-        Strip off the scope operator if exists in name.
+        Strip off the scope operator if it exists in name.
 
         """
         if (self._name and self.scope_operator and
@@ -173,7 +221,20 @@ class Style(collections.OrderedDict, qstylizer.setter.prop.PropSetter):
         return self._parent
 
     @property
+    def is_root(self):
+        return self._is_root
+
+    @property
     def scope_operator(self):
+        """Get the scope operator.
+
+        The scope operator is the "::" or ":" that is printed in front
+        of the name of the Style in the identifier.
+
+        Subclasses are expected to define the scope operator or else it will
+        try to guess it based on its position in the hierarchy.
+
+        """
         if self.is_top_level():
             return ""
         elif self.is_leaf():
@@ -191,11 +252,8 @@ class Style(collections.OrderedDict, qstylizer.setter.prop.PropSetter):
     def is_top_level(self):
         return self._parent.is_root
 
-    @property
-    def is_root(self):
-        return self._is_root
-
     def style(self):
+        """Return the identifier and properties as a single string."""
         properties = ""
         sheet = ""
         identifier = self.identifier
@@ -207,6 +265,7 @@ class Style(collections.OrderedDict, qstylizer.setter.prop.PropSetter):
         return sheet
 
     def stylesheet(self):
+        """Return a stylesheet as string containing the entire hierarchy."""
         stylesheet = self.style()
         for key, value in self.items():
             if isinstance(value, Style):
@@ -223,30 +282,54 @@ class Style(collections.OrderedDict, qstylizer.setter.prop.PropSetter):
 class ClassStyle(Style,
                  qstylizer.setter.subcontrol.SubControlSetter,
                  qstylizer.setter.pseudostate.PseudoStateSetter):
-    pass
+    """The ClassStyle definition.
+
+    Example class style name: "QCheckBox".
+    Contains descriptors for all subcontrols and pseudostates.
+
+    """
 
 
 class ObjectStyle(ClassStyle):
+    """The ClassStyle definition.
 
+    Example object style name: "#objectName".
+    Inherits from ClassStyle. Only difference is "#" is the scope operator.
+
+    """
     @property
     def scope_operator(self):
         return "#"
 
 
 class ObjectProperty(Style):
+    """The ObjectProperty definition.
 
+    Example object property style name: "[echoMode="2"]".
+
+    """
     @property
     def scope_operator(self):
         return ""
 
 
 class StyleList(Style):
+    """The StyleList definition.
 
+    Example style list name: "QCheckBox, QComboBox".
+
+    """
     @property
     def scope_operator(self):
         return ""
 
     def __setattr__(self, name, val):
+        """Override the setting of an attribute.
+
+        Will loop through all components in name separated by a comma and set the
+        property in each of the substyles in the parent Style.
+
+        """
         if name.startswith("_"):
             return super(Style, self).__setattr__(name, val)
         style_names = self.name.split(",")
@@ -256,19 +339,27 @@ class StyleList(Style):
 
     @property
     def name(self):
-        name = self._name
-        return name.replace(" ", "")
+        """Return the name with no spaces."""
+        return self._name.replace(" ", "")
 
 
 class SubControl(Style, qstylizer.setter.pseudostate.PseudoStateSetter):
+    """The SubControl definition.
 
+    Example subcontrol name: "::indicator".
+
+    """
     @property
     def scope_operator(self):
         return "::"
 
 
 class PseudoState(Style):
+    """The PseudoState definition.
 
+    Example pseudostate name: ":hover".
+
+    """
     @property
     def scope_operator(self):
         return ":"
