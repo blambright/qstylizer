37638a3aeb6235c1a19f090411a2d24fc011fb40
Brett Lambright <brettpl@gmail.com>
2017-11-12 21:23:40 -0800

    Implement initial functionality of parser with the help of tinycss

---
+++ qstylizer/parser.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import tinycss
+
+import qstylizer.style
+
+
+def parse_stylesheet(stylesheet):
+    """Parse a stylesheet using tinycss and return a StyleSheet instance.
+
+    :param stylesheet: A string of an existing stylesheet.
+
+    """
+    parsed_stylesheet = tinycss.make_parser().parse_stylesheet(stylesheet)
+
+    css = qstylizer.style.StyleSheet()
+    for rule in parsed_stylesheet.rules:
+        selector = rule.selector.as_css()
+        for declaration in rule.declarations:
+            prop = declaration.name
+            value = declaration.value.as_css()
+            css[selector].__setattr__(prop, value)
+    return css
