1bbd217d1ef79a5915a00ae378c8bf3c587b5da8
Ben Greiner <code@bnavigator.de>
2021-04-24 14:48:17 +0200

    only strip the ends

--- qstylizer/parser.py
+++ qstylizer/parser.py
@@ -5,18 +5,6 @@ import tinycss2
 import qstylizer.style
 
 
-def _strip_whitespace_recursive(value):
-    for i, v in enumerate(value):
-        if isinstance(v, list):
-            value[i].value = _strip_whitespace_recursive(v)
-        else:
-            try:
-                value[i].value = v.value.strip()
-            except AttributeError:
-                pass
-    return value
-
-
 def parse(stylesheet):
     """Parse a stylesheet using tinycss2 and return a StyleSheet instance.
 
@@ -30,14 +18,12 @@ def parse(stylesheet):
     for node in parsed_stylesheet:
         if node.type == "error":
             raise ValueError("Cannot parse Stylesheet: " + node.message)
-        selector = tinycss2.serialize(_strip_whitespace_recursive(node.prelude))
+        selector = tinycss2.serialize(node.prelude).strip()
         declaration_list = tinycss2.parse_declaration_list(
             node.content, skip_comments=True, skip_whitespace=True
         )
         for declaration in declaration_list:
             if declaration.type == "declaration":
                 prop = declaration.name.strip()
-                v = _strip_whitespace_recursive(declaration.value)
-                value = tinycss2.serialize(v)
-                css[selector][prop] = value
+                css[selector][prop] = tinycss2.serialize(declaration.value).strip()
     return css
